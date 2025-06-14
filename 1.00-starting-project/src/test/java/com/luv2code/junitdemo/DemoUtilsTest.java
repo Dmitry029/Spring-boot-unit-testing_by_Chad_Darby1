45ee9eda0e7d95dfc08aa23752ff6ea4014bd143
Dmitry029 <kdp.029029@cmail.com>
2025-06-14 22:06:40 +0300

    add DisplayNameGenerator

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,35 +4,23 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 public class DemoUtilsTest {
 
     private DemoUtils demoUtils;
 
-    @BeforeAll
-    public static void setUpClass() {
-        System.out.println("@BeforeAll executes only once before all test methods");
-    }
-
-    @AfterAll
-    public static void tearDownClass() {
-        System.out.println("@AfterAll executes only once after all test methods");
-    }
-
     @BeforeEach
     void setUpBeforeEach() {
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach executes before the execution of each test");
     }
 
     @AfterEach
     void tearDownAfterEach() {
-        System.out.println("@AfterEach executes after the execution of each test");
         System.out.println();
     }
 
     @Test
     void testEqualsAndNotEquals() {
-        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1+9 must be 6");
@@ -40,7 +28,6 @@ public class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
-        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "luv2code";
@@ -49,3 +36,13 @@ public class DemoUtilsTest {
         assertNotNull(demoUtils.checkNull(str2), "object should not be null");
     }
 }
+/* @BeforeAll
+    public static void setUpClass() {
+        System.out.println("@BeforeAll executes only once before all test methods");
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+        System.out.println("@AfterAll executes only once after all test methods");
+    }*/
+
